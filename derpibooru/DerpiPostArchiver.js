c9652992c0bb9d39315876adae70692dbdb1dbe4
Eievui <62132046+LightEievui@users.noreply.github.com>
2023-07-05 01:11:09 -0500

    Test auto update

--- derpibooru/DerpiPostArchiver.js
+++ derpibooru/DerpiPostArchiver.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Derpi Post Archiver
 // @namespace    Eievui
-// @version      0.1
+// @version      0.2
 // @description  Archive / bookmark derpibooru comments and forum posts to easily view later for funny or helpful posts
 // @author       Eievui
 // @homepageURL  https://derpibooru.org/profiles/Eievui
@@ -21,6 +21,8 @@ This does not archive badges because each saved badge would've added another ~0.
 Archive button will be under each comment and forum post
 View your archived from user dropdown list
 */
+
+// Remove
 
 (function() {
   "use strict";
